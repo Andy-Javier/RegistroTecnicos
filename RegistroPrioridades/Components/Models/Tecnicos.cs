fe24e21dbb940adfed89ee4f0234e21123adc598
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-08-31 23:33:49 -0400

    Cambio de Clave a TecnicoId

--- RegistroPrioridades/Components/Models/Tecnicos.cs
+++ RegistroPrioridades/Components/Models/Tecnicos.cs
@@ -2,10 +2,11 @@
 
 namespace RegistroTecnicos.Models
 {
-public class Tecnicos
-{
-    [Key]
-        public int TecnicosId { get; set; }
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+
         [Required(ErrorMessage = "El Campo Descripci&oacute;n es obligatorio")]
         public string? Descripcion { get; set; }
     }
