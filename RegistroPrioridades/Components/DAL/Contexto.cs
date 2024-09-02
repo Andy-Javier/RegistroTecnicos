a345969f9ed818339c7b71f2b0bc6018c1702f78
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-09-01 23:37:44 -0400

    Modificacion del contexto, agregando definiciones para tecnicos

--- RegistroPrioridades/Components/DAL/Contexto.cs
+++ RegistroPrioridades/Components/DAL/Contexto.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Models;
 
-namespace RegistroTecnicos.DAL
+public class Contexto : DbContext
 {
-    public class Contexto : DbContext
+    public DbSet<Tecnicos> Tecnicos { get; set; }
+
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
     {
-        public Contexto(DbContextOptions<Contexto> options)
-            : base(options) { }
-        public DbSet<Tecnicos> Tecnicos { get; set; }
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data Source=path_to_your_database.db");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tecnicos>(entity =>
+        {
+            entity.HasIndex(e => e.Nombre).IsUnique(); // Para evitar nombres duplicados
+        });
     }
 }
