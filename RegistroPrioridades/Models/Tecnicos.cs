8a21a9ef95d5be27de29aa7d64be7090a5c6d154
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-09-07 21:00:41 -0400

    Moví el componente Tecnicos.cs

---
+++ RegistroPrioridades/Models/Tecnicos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal SueldoHora { get; set; }
+    }
+}
