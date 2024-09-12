2a5cce872a5c6a7e38887f74615288cdfc869b00
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-09-12 13:33:39 -0400

    Creacion de TiposTecnicos.cs

---
+++ RegistroPrioridades/Models/TiposTecnicos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class TiposTecnicos
+    {
+        [Key]
+
+        public int TipoTecnicoId { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [RegularExpression(@"[a-zA-Z\s]+$", ErrorMessage = "Solo son permitidas las letras")]
+        public string? Descripcion { get; set; }
+
+    }
+}
