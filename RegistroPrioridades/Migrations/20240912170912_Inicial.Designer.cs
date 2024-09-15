ed83682229db4291f74b86eb439ab96c382e3555
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-09-15 01:17:01 -0400

    migraciones para Trabajos y Clientes

--- RegistroPrioridades/Migrations/20240912170912_Inicial.Designer.cs
+++ RegistroPrioridades/Migrations/20240912170912_Inicial.Designer.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using RegistroTecnicos.Pages.DAL;
+using RegistroTecnicos.DAL;
 
 #nullable disable
 
--- RegistroPrioridades/Migrations/ContextoModelSnapshot.cs
+++ RegistroPrioridades/Migrations/ContextoModelSnapshot.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using RegistroTecnicos.Pages.DAL;
+using RegistroTecnicos.DAL;
 
 #nullable disable
 
