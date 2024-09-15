0c367b168bf9c9081e5d94bfed2ea1aa553b0d9d
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-09-15 01:16:31 -0400

    Cambios en el Contexto para añadir Clientes y Trabajos, ademas de una correccion

--- RegistroPrioridades/DAL/Contexto.cs
+++ RegistroPrioridades/DAL/Contexto.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Models;
 
-namespace RegistroTecnicos.Pages.DAL
-{
+namespace RegistroTecnicos.DAL;
+
     public class Contexto : DbContext
     {
 
@@ -11,5 +11,6 @@ namespace RegistroTecnicos.Pages.DAL
 
         public DbSet<Tecnicos> Tecnicos { get; set; }
         public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Trabajos> Trabajos { get; set; }
     }
-}
