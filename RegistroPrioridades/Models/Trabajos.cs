df2b60d8320f6f21c9d5e3b3f87e5947e912521b
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-09-15 01:17:50 -0400

    Creacion de Trabajos.cs

---
+++ RegistroPrioridades/Models/Trabajos.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using RegistroTecnicos.Models;
+
+namespace RegistroTecnicos.DAL
+{
+    public class Trabajos
+    {
+        [Key]
+        public int TrabajoId { get; set; }
+        [Required]
+        public DateTime Fecha { get; set; }
+        [Required]
+        public int ClienteId { get; set; }
+        [Required]
+        public int TecnicoId { get; set; }
+        [Required]
+        public string? Descripcion { get; set; }
+        [Required]
+        public decimal Monto { get; set; }
+        [ForeignKey("ClienteId")]
+        public Clientes? Cliente { get; set; }
+        public Tecnicos? Tecnico { get; set; }
+    }
+}
