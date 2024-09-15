cc996321be320f05a33afa9cec82253980a3f917
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-09-15 01:17:34 -0400

    Creacion de Clientes.cs

---
+++ RegistroPrioridades/Models/Clientes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string? Nombres { get; set; }
+        [Required(ErrorMessage = "El número es requerido")]
+        public string? WhatsApp { get; set; }
+    }
+
+}
