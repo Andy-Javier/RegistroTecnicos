d65fd06b91e4829444403df82b1fbb340f124e0e
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-10-18 23:27:21 -0400

    añadì el Services de Articulos

---
+++ RegistroPrioridades/Services/ArticulosServices.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+namespace RegistroTecnicos.Services;
+
+public class ArticulosServices
+{
+    private readonly DbContext _context;
+
+    public ArticulosServices(DbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Guardar(Articulos articulo)
+    {
+        if (articulo.ArticuloId == 0)
+            _context.Set<Articulos>().Add(articulo);
+        else
+            _context.Set<Articulos>().Update(articulo);
+
+        return await _context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<Articulos> Buscar(int id)
+    {
+        return await _context.Set<Articulos>().FindAsync(id);
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        var articulo = await _context.Set<Articulos>().FindAsync(id);
+        if (articulo != null)
+        {
+            _context.Set<Articulos>().Remove(articulo);
+            return await _context.SaveChangesAsync() > 0;
+        }
+        return false;
+    }
+
+    public async Task<List<Articulos>> Listar()
+    {
+        return await _context.Set<Articulos>().ToListAsync();
+    }
+}
