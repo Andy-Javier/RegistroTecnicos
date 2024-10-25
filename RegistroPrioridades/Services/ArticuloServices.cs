18a06113488902a2b963d076acef88d5f67cd06d
Andy Javier <andy_javier@ucne.edu.do>
2024-10-24 21:58:30 -0400

    modifiqué el ArticuloServices para agregar el ContextoFactory

--- RegistroPrioridades/Services/ArticuloServices.cs
+++ RegistroPrioridades/Services/ArticuloServices.cs
@@ -3,53 +3,59 @@ using RegistroTecnicos.DAL;
 using RegistroTecnicos.Models;
 using System.Linq.Expressions;
 
-namespace RegistroTecnicos.Services;
-
-public class ArticuloServices
+namespace RegistroTecnicos.Services
 {
-    private readonly Contexto _contexto;
-
-    public ArticuloServices(Contexto contexto)
+    public class ArticuloServices
     {
-        _contexto = contexto;
-    }
+        private readonly ContextoFactory _contextoFactory;
 
-    public async Task<List<Articulos>> ListaArticulos()
-    {
-        return await _contexto.Articulos
-            .AsNoTracking()
-            .ToListAsync();
-    }
-
-    public async Task<Articulos?> ObtenerArticuloPorId(int id)
-    {
-        return await _contexto.Articulos
-            .AsNoTracking()
-            .FirstOrDefaultAsync(a => a.ArticuloId == id);
-    }
-
-    public async Task ActualizarExistencia(int articuloId, decimal cantidad)
-    {
-        var articulo = await _contexto.Articulos.FindAsync(articuloId);
-        if (articulo != null)
+        public ArticuloServices(ContextoFactory contextoFactory)
         {
-            articulo.Existencia -= (int)cantidad;
-            _contexto.Articulos.Update(articulo);
-            await _contexto.SaveChangesAsync();
+            _contextoFactory = contextoFactory;
+        }
+
+        private Contexto GetContext()
+        {
+            return _contextoFactory.CreateDbContext(new string[] { });
+        }
+
+        public async Task<List<Articulos>> ListaArticulos()
+        {
+            using var _contexto = GetContext();
+            return await _contexto.Articulos
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Articulos?> ObtenerArticuloPorId(int id)
+        {
+            using var _contexto = GetContext();
+            return await _contexto.Articulos
+                .AsNoTracking()
+                .FirstOrDefaultAsync(a => a.ArticuloId == id);
+        }
+
+        public async Task ActualizarExistencia(int articuloId, decimal cantidad)
+        {
+            using var _contexto = GetContext();
+            var articulo = await _contexto.Articulos.FindAsync(articuloId);
+            if (articulo != null)
+            {
+                articulo.Existencia -= (int)cantidad;
+                _contexto.Articulos.Update(articulo);
+                await _contexto.SaveChangesAsync();
+            }
+        }
+
+        public async Task AgregarCantidad(int articuloId, int cantidad)
+        {
+            using var _contexto = GetContext();
+            var articulo = await _contexto.Articulos.FindAsync(articuloId);
+            if (articulo != null)
+            {
+                articulo.Existencia += cantidad;
+                await _contexto.SaveChangesAsync();
+            }
         }
     }
-
-
-    public async Task AgregarCantidad(int articuloId, int cantidad)
-    {
-        var articulo = await _contexto.Articulos.FindAsync(articuloId);
-
-        if (articulo != null)
-        {
-            articulo.Existencia += cantidad;
-
-            await _contexto.SaveChangesAsync();
-        }
-    }
-
 }
