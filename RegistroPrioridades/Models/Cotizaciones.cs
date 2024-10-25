7c082fcad0766e850ab46e17c515c4e64d3a3458
Andy Javier <andy_javier@ucne.edu.do>
2024-10-24 22:54:40 -0400

    Cree cotizaciones

---
+++ RegistroPrioridades/Models/Cotizaciones.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Cotizaciones
+    {
+        [Key]
+        public int CotizacionId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int ClienteId { get; set; }
+        public string? Observacion { get; set; }
+        public decimal Monto { get; set; }
+    }
+}
