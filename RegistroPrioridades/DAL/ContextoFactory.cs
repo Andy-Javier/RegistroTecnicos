6c351c195b766cd9eaf58f5cc7376b5168757c96
Andy Javier <andy_javier@ucne.edu.do>
2024-10-24 21:54:18 -0400

    cree el ContextoFactory

---
+++ RegistroPrioridades/DAL/ContextoFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using RegistroTecnicos.DAL;
+using System.IO;
+
+public class ContextoFactory : IDesignTimeDbContextFactory<Contexto>
+{
+    public Contexto CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<Contexto>();
+        optionsBuilder.UseSqlite("Data Source=app.db");
+
+        return new Contexto(optionsBuilder.Options);
+    }
+}
