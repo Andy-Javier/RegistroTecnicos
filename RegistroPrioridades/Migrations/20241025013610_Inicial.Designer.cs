4f91add3dce545610dce284aea081d48948c7d1e
Andy Javier <andy_javier@ucne.edu.do>
2024-10-24 21:55:10 -0400

    nuevas migraciones

--- RegistroPrioridades/Migrations/20241025013610_Inicial.Designer.cs
+++ RegistroPrioridades/Migrations/20241025013610_Inicial.Designer.cs
@@ -11,7 +11,7 @@ using RegistroTecnicos.DAL;
 namespace RegistroTecnicos.Migrations
 {
     [DbContext(typeof(Contexto))]
-    [Migration("20241020021106_Inicial")]
+    [Migration("20241025013610_Inicial")]
     partial class Inicial
     {
         /// <inheritdoc />
